a7d5abf5c20e5a58993a9d7d59dd3284e28133c9
yerassylrymkul <rymkulerasyl@gmail.com>
2024-08-27 19:09:21 +0500

    delete

--- internal/domain/auth/entity.go
+++
@@ -1,6 +0,0 @@
-package auth
-
-type Entity struct {
-	Email    *string `db:"email"`
-	Password *string `db:"password"`
-}
--- internal/domain/auth/repository.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "context"
-
-type Repository interface {
-	SignUp(ctx context.Context, data Entity) (id string, err error)
-	//SignIn(ctx context.Context, id string) (dest Entity, err error)
-}
